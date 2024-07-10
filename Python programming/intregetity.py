99444eb6cf7071757c45b235c7c6ff0364594c3e
Archisman Gupta <workshome97@gmail.com>
2024-07-10 12:11:39 +0530

    Add files via upload

---
+++ Python programming/intregetity.py
@@ -0,0 +1,7 @@
+a=70
+b=77
+print(a+3)
+if b>10:
+    print ("the value of more than 70 initially.")
+else:
+    print("the value is 70  or less initrygrety.")
---
+++ Python programming/variableprint.py
@@ -0,0 +1,6 @@
+from sys import argv 
+script= first, second, third = argv
+print("the script is called",script)
+print("the value of",first)
+print("second variable is:",second)
+print("third variable is:",third)
